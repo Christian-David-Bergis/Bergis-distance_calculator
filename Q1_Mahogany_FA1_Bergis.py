c47548000ab7fbc72b9b4261c1e7d3505772dfee
Christian-David-Bergis <cdbbergis31@src.pshs.edu.ph>
2026-08-13 10:02:26 +0800

    Add files via upload

---
+++ Q1_Mahogany_FA1_Bergis.py
@@ -0,0 +1,14 @@
+ # Enter the required value of the first coordinates
+x_coordinate1 = float(input("Enter x_coordinate1: "))
+y_coordinate1 = float(input("Enter y_coordinate1: "))
+
+# Enter the required value of the second coordinates
+x_coordinate2 = float(input("Enter x_coordinate2: "))
+y_coordinate2 = float(input("Enter y_coordinate2: "))
+
+# Compute the given values
+import math
+d= math.square_root(math.power(x2 - x1, 2) + math.power(y2 - y1, 2))
+
+# Display the computed result
+print ("The distance is", d)
